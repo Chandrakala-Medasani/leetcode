9824e0448cccbd00f5633d48f101a8e594aa84bf
Chandrakala-Medasani <chandum0409@gmail.com>
2026-03-07 16:10:09 +0530

    Implement strStr function to find substring index

---
+++ 28strStr.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i=0; i<=haystack.length()-needle.length(); i++){
+            String s = haystack.substring(i,i+needle.length());
+            if(s.contains(needle))
+                return i;
+        }
+        return -1;
+    }
+}
